247ba1b15bb8e7f77d44e8c51a9f90df2d082e04
Diego Vistoso <dietroxshadow14@gmail.com>
2022-11-17 01:42:21 -0300

    Update index.js
    agregar tooltips

--- js/index.js
+++ js/index.js
@@ -37,3 +37,10 @@ anime.timeline({loop: true})
     easing: "easeOutExpo",
     delay: 1000
   });
+
+const tooltipTriggerList = document.querySelectorAll(
+  '[data-bs-toggle="tooltip"]'
+);
+const tooltipList = [...tooltipTriggerList].map(
+  (tooltipTriggerEl) => new bootstrap.Tooltip(tooltipTriggerEl)
+);
